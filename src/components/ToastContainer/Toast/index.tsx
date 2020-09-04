eb6f4cb170e5a897c29ea1553421203afb0a4b44
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-09-04 18:11:24 -0300

    anima os toasts

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -12,6 +12,8 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  style: object;
 }
 
 const icons = {
@@ -20,7 +22,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -35,7 +37,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
