e4f0798dd1b12ecae0de523f90bbb882f67ca625
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-10-17 22:22:17 -0300

    proximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,35 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQEe_ckn7iJvRg/profile-displayphoto-shrink_800_800/0?e=1608768000&v=beta&t=9H_1m-NuBrCesnRLUJVzTVGD0bzVr6lPyvScojWSaSw"
+                alt="Livia Carneiro"
+              />
+
+              <strong>Livia Carneiro</strong>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
