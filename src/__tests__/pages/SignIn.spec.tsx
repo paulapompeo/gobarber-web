88340959fd1d008184e8f12692c44f16a48cdb46
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-11-14 13:22:36 -0300

    teste de login

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+// descobrir se o history foi chamado
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be able to SignIn', () => {
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+
+    const buttonElement = getByText('Entrar');
+
+    // preencher valores para os campos de email e senha (simular evento)
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    // simular evento de click
+    fireEvent.click(buttonElement);
+
+    // acao visual que ocorre apos as acoes acima: redireciona para outra pagina
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
-
